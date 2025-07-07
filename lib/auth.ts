6aab3235c8a81f007d3ff1ba773bc2df8f9bea36
Ashwin <bhanageashwin28@gmail.com>
2025-07-07 22:16:07 +0530

    Connect to mongoDb and changes in DB file

---
+++ lib/auth.ts
@@ -0,0 +1,65 @@
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+import connectToDatabase from "./db";
+import User from "@/models/User";
+import bcrypt from "bcryptjs";
+
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        }),
+        GitHubProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        }),
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+                username: { label: "Username", type: "text"},
+                email: {label: 'Email', type: 'email'},
+                password: {label: 'Password',type: 'password'}
+            },
+
+            async authorize(credentials){
+                if(!credentials || !credentials.email || !credentials.password){
+                    throw new Error('Invalid credentials');
+                }
+
+                try {
+                    await connectToDatabase();
+                    const user = await User.findOne({email: credentials.email});
+
+                    if(!user){
+                        throw new Error('User not found');
+                    }
+
+                    const isValid = await bcrypt.compare(
+                        credentials.password,
+                        user.password
+                    );
+
+                    if(!isValid){
+                        throw new Error('Invalid password');
+                    }
+
+
+                    return {
+                        id: user._id.toString(),
+                        name: user.name,
+                        email: user.email
+                    }
+
+
+                } catch (error) {
+                    console.error("Console Auth Error!",error);
+                    throw error
+                }
+            }
+        })
+    ]
+}
--- lib/db.ts
+++ lib/db.ts
@@ -13,30 +13,26 @@ if (!cached) {
     cached = global.mongoose = { conn: null, promise: null }
 }
 
-export async function dbConnect() {
+export async function connectToDatabase() {
     if (cached.conn) {
-        return cached.conn
+    return cached.conn;
     }
 
     if (!cached.promise) {
-        
-        // opts is used to configure the connection options for mongoose.
-        const opts = {
-            bufferCommands: false, // Disable mongoose's buffering of commands
-            maxPoolSize: 10, // Set the maximum pool size to 10 used for multiple connnections of db.
-        }
-        
-        mongoose
-        .connect(MONGODB_URI,opts)
-        .then(() => mongoose.connection)
+    const opts = {
+        bufferCommands: true,
+        maxPoolSize: 10,
+    };
+
+    mongoose.connect(MONGODB_URI, opts).then(() => mongoose.connection);
     }
 
     try {
-        cached.conn = await cached.promise
+    cached.conn = await cached.promise;
     } catch (error) {
-        cached.promise = null
-        throw error;
+    cached.promise = null;
+    throw error;
     }
 
-    return cached.conn
-}
+    return cached.conn;
+    }
