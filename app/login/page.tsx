eb55e67e7567a0a9de9b84c05d2b5eff9f72bc04
Ashwin <bhanageashwin28@gmail.com>
2025-07-11 01:11:36 +0530

    LoginForm Creation Complete

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,51 @@
-import React from 'react'
+"use client";
+import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react'
 
-function LoginForm
-() {
-  return (
-    <div>LoginForm
+function LoginForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {e.preventDefault();
+    const res = await signIn('credentials', {
+      email,
+      password,
+      redirect: false
+    });
+
+    if(res?.error) {
+      console.error("Login failed:");
+    }else{
+      router.push('/');
+    }
+};
+
+return(
+  <div>
+    <h1>Login Form</h1>
+    <form onSubmit={handleSubmit}>
+      <input 
+      type="email"
+      placeholder='Email'
+      value={email}
+      onChange={(e) => setEmail(e.target.value)} 
+      />
+      <input 
+      type="password"
+      placeholder='Password'
+      value={password}
+      onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type='submit'>Login</button>
+    </form>
+
+    <div>
+      Dont have an account? <button onClick={() => router.push('/register')}>Register</button>
     </div>
-  )
+  </div>
+  );
 }
 
-export default LoginForm
+export default LoginForm;
