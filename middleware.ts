6c914332e05d74671e4f28de8d18c8a9acb86ca5
Ashwin <bhanageashwin28@gmail.com>
2025-07-12 23:14:59 +0530

    minor changes

--- middleware.ts
+++ middleware.ts
@@ -3,26 +3,26 @@ import { NextResponse } from "next/server";
 
 export default withAuth(
     function middleware() {
-    return NextResponse.next();
+        return NextResponse.next();
     },
     {
-    callbacks: {
-        authorized({ req, token }) {
-        const { pathname } = req.nextUrl;
-        if (
-            pathname.startsWith("/api/auth") ||
-            pathname === "/login" ||
-            pathname === "/register"
-        )
-            return true;
+        callbacks: {
+            authorized({ req, token }) {
+            const { pathname } = req.nextUrl;
+            if (
+                pathname.startsWith("/api/auth") ||
+                pathname === "/login" ||
+                pathname === "/register"
+            )
+                return true;
 
-        if (pathname === "/" || pathname.startsWith("/api/videos")) {
-            return true;
-        }
+            if (pathname === "/" || pathname.startsWith("/api/videos")) {
+                return true;
+            }
 
-        return !!token;
+            return !!token;
+            },
         },
-    },
     }
 );
 
